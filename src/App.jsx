558fff1088452d0deeff583a5cacb22a3d9712c8
Ambuj Agrawal <ambuj.agrawal@cred.club>
2021-05-27 20:47:58 +0530

    added auth firebase google

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,18 @@ import React from 'react';
 import './App.css';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import Snackbar from '@material-ui/core/Snackbar';
-
+import { StyledFirebaseAuth } from 'react-firebaseui';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import firebase from 'firebase/app';
 import MainApp from './MainApp';
 import ApiHandlerProvider from './provider/ApiHandlerProvider';
 import Firebase from './Firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyCbGWlv_6igZpLGLQLGx5wKr1Ufd6Lv0ZI',
+  authDomain: 'o2-tracker.firebaseapp.com',
+  projectId: 'o2-tracker',
+});
 
 const theme = createMuiTheme({
   palette: {
@@ -37,27 +45,48 @@ const theme = createMuiTheme({
 });
 
 class App extends React.Component {
+  uiconfig = {
+    signInFlow: 'popup',
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    ],
+    callbacks: {
+      signInSuccess: () => false,
+    },
+  };
+
   constructor(props) {
     super(props);
-    this.state = { user: true, toast: '' };
+    this.state = { user: null, toast: '' };
     this.firebase = new Firebase();
   }
 
   componentDidMount = () => {
     this.firebase.setSignInListener(
       (user) => {
+        console.log(user);
         this.setState({ user });
       },
-      () => {
+      (errorMessage) => {
         // error maybe due to user not having access, network error etc
+        this.setState({ toast: errorMessage });
       },
     );
   }
 
   render() {
     const { user } = this.state;
-    let screen = <h1>HomePage</h1>;
+    let screen = (
+      <div>
+        <StyledFirebaseAuth
+          uiConfig={this.uiconfig}
+          firebaseAuth={firebase.auth()}
+        />
+        <div><p> Helloooo</p></div>
+      </div>
+    );
     if (user) {
+      console.log('logged in ', user);
       screen = (
         <ApiHandlerProvider
           firebaseHandler={this.firebase}
--- src/Firebase.js
+++ src/Firebase.js
@@ -1,25 +1,79 @@
+/* eslint-disable no-console */
+/* eslint-disable import/no-extraneous-dependencies */
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
 export default class Firebase {
   constructor() {
     this.user = null;
   }
+
+ auth = firebase.auth();
+
+firestore = firebase.firestore();
+
+checkIfAdminExist = async (user) => {
+  const userRef = this.firestore.doc(`admins/${user.uid}`);
+  const snapshot = await userRef.get();
+  return snapshot.exists;
+};
 
   signIn = () => {
     this.onSignInChanged(true);
   }
 
   signOut = () => {
-    this.onSignInChanged(null);
+    this.auth.signOut();
   }
 
+  getConfig = () => (
+    {
+      signInFlow: 'popup',
+      signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      ],
+      callbacks: {
+        signInSuccess: () => false,
+      },
+    }
+  )
+
   setSignInListener = (onSignInChanged, onSignInFailed) => {
+    console.log(this.auth, this.firestore);
     this.onSignInChanged = onSignInChanged;
     this.onSignInFailed = onSignInFailed;
+    this.auth.onAuthStateChanged(async (user) => {
+      console.log('user is ', user);
+      if (!user) {
+        this.onSignInChanged(null);
+        return null;
+      }
+      const doesExist = await this.checkIfAdminExist(user);
+      if (doesExist) {
+        this.onSignInChanged(user);
+      } else {
+        this.onSignInFailed('Contact authority, not a registered admin');
+        firebase.auth().signOut();
+      }
+      return null;
+      // if (this.state.isSignedIn && this.state.isValidAdmin) {
+      //   const allUsers = await getAllUsersData()
+
+      //   const allCylinders = await getAllCylindersData()
+      //   const allCitizens =  await getAllCitizens()
+
+      //   const cylinderData = getCyclinderTableData(  allUsers,   allCylinders,   allCitizens)
+      //   console.log("cyclinder data for table is ", cylinderData)
+      //   this.setState({ data:cylinderData})
+      // }
+    });
   }
 
   getUser = () => ({
-    name: 'Rahul Jha',
-    email: 'jharahul1998@gmail.com',
-    avatarUrl: 'https://i.pinimg.com/originals/51/f6/fb/51f6fb256629fc755b8870c801092942.png',
+    name: this.auth.currentUser.displayName,
+    email: this.auth.currentUser.email,
+    avatarUrl: this.auth.currentUser.photoURL,
   })
 
   addUser = (phoneNo, payload) => ({ phoneNo: payload })
